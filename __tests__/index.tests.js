afb0e1302afba5c21b6c46f95bb363c7b2cef425
Anton Zlobin <zlobinanton20@gmail.com>
2022-09-07 16:19:18 +0500

    add plain test

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -1,54 +1,21 @@
 import { expect, test } from '@jest/globals';
-import engineDiff from '../index';
-
-const correctDiff = '{\n'
-                       + '    common: {\n'
-                       + '      + follow: false\n'
-                       + '        setting1: Value 1\n'
-                       + '      - setting2: 200\n'
-                       + '      - setting3: true\n'
-                       + '      + setting3: null\n'
-                       + '      + setting4: blah blah\n'
-                       + '      + setting5: {\n'
-                       + '            key5: value5\n'
-                       + '        }\n'
-                       + '        setting6: {\n'
-                       + '            doge: {\n'
-                       + '              - wow: \n'
-                       + '              + wow: so much\n'
-                       + '            }\n'
-                       + '            key: value\n'
-                       + '          + ops: vops\n'
-                       + '        }\n'
-                       + '    }\n'
-                       + '    group1: {\n'
-                       + '      - baz: bas\n'
-                       + '      + baz: bars\n'
-                       + '        foo: bar\n'
-                       + '      - nest: {\n'
-                       + '            key: value\n'
-                       + '        }\n'
-                       + '      + nest: str\n'
-                       + '    }\n'
-                       + '  - group2: {\n'
-                       + '        abc: 12345\n'
-                       + '        deep: {\n'
-                       + '            id: 45\n'
-                       + '        }\n'
-                       + '    }\n'
-                       + '  + group3: {\n'
-                       + '        deep: {\n'
-                       + '            id: {\n'
-                       + '                number: 45\n'
-                       + '            }\n'
-                       + '        }\n'
-                       + '        fee: 100500\n'
-                       + '    }\n'
-                       + '}';
+import engineDiff from '../index.js';
+import {
+  correctStylishDiff,
+  correctPlainDiff,
+}
+  from './correctResult.js'
 
 test('stylish test', () => {
   const filepath1 = 'file1.json';
   const filepath2 = 'file2.json';
 
-  expect(engineDiff(filepath1, filepath2)).toBe(correctDiff);
+  expect(engineDiff(filepath1, filepath2)).toBe(correctStylishDiff);
+});
+
+test('plain test', () => {
+  const filepath1 = 'file1.json';
+  const filepath2 = 'file2.json';
+
+  expect(engineDiff(filepath1, filepath2)).toBe(correctPlainDiff);
 });
