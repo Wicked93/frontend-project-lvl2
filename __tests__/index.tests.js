ca4159cc864b1d4b3e42ca7ba8f9980a35073887
Anton Zlobin <zlobinanton20@gmail.com>
2022-09-07 15:44:22 +0500

    add stylish test

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -1,6 +1,54 @@
 import { expect, test } from '@jest/globals';
 import engineDiff from '../index';
 
-test('hhd', () => {
-  expect(engineDiff(filepath2.json, filepath2.json)).toEqual(`- follow: false\nhost: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true`);
+const correctDiff = '{\n'
+                       + '    common: {\n'
+                       + '      + follow: false\n'
+                       + '        setting1: Value 1\n'
+                       + '      - setting2: 200\n'
+                       + '      - setting3: true\n'
+                       + '      + setting3: null\n'
+                       + '      + setting4: blah blah\n'
+                       + '      + setting5: {\n'
+                       + '            key5: value5\n'
+                       + '        }\n'
+                       + '        setting6: {\n'
+                       + '            doge: {\n'
+                       + '              - wow: \n'
+                       + '              + wow: so much\n'
+                       + '            }\n'
+                       + '            key: value\n'
+                       + '          + ops: vops\n'
+                       + '        }\n'
+                       + '    }\n'
+                       + '    group1: {\n'
+                       + '      - baz: bas\n'
+                       + '      + baz: bars\n'
+                       + '        foo: bar\n'
+                       + '      - nest: {\n'
+                       + '            key: value\n'
+                       + '        }\n'
+                       + '      + nest: str\n'
+                       + '    }\n'
+                       + '  - group2: {\n'
+                       + '        abc: 12345\n'
+                       + '        deep: {\n'
+                       + '            id: 45\n'
+                       + '        }\n'
+                       + '    }\n'
+                       + '  + group3: {\n'
+                       + '        deep: {\n'
+                       + '            id: {\n'
+                       + '                number: 45\n'
+                       + '            }\n'
+                       + '        }\n'
+                       + '        fee: 100500\n'
+                       + '    }\n'
+                       + '}';
+
+test('stylish test', () => {
+  const filepath1 = 'file1.json';
+  const filepath2 = 'file2.json';
+
+  expect(engineDiff(filepath1, filepath2)).toBe(correctDiff);
 });
