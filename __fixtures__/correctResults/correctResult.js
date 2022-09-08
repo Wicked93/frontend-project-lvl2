c1d845dec3b003439df77eb3aaaaf4ee5969eccb
Anton Zlobin <zlobinanton20@gmail.com>
2022-09-08 17:06:30 +0500

    add JSON test

--- __fixtures__/correctResults/correctResult.js
+++ __fixtures__/correctResults/correctResult.js
@@ -54,7 +54,9 @@ const correctPlainDiff = "Property 'common.follow' was added with value: false\n
                        + "Property 'group1.nest' was updated. From [complex value] to 'str'\n"
                        + "Property 'group2' was removed\n"
                        + "Property 'group3' was added with value: [complex value]";
+const correctJSONDiff = '[{"key":"common","type":"object","children":[{"key":"follow","type":"added","value":false},{"key":"setting1","type":"unchanged","value":"Value 1"},{"key":"setting2","type":"deleted","value":200},{"key":"setting3","type":"changed","value":null,"oldValue":true},{"key":"setting4","type":"added","value":"blah blah"},{"key":"setting5","type":"added","value":{"key5":"value5"}},{"key":"setting6","type":"object","children":[{"key":"doge","type":"object","children":[{"key":"wow","type":"changed","value":"so much","oldValue":""}]},{"key":"key","type":"unchanged","value":"value"},{"key":"ops","type":"added","value":"vops"}]}]},{"key":"group1","type":"object","children":[{"key":"baz","type":"changed","value":"bars","oldValue":"bas"},{"key":"foo","type":"unchanged","value":"bar"},{"key":"nest","type":"changed","value":"str","oldValue":{"key":"value"}}]},{"key":"group2","type":"deleted","value":{"abc":12345,"deep":{"id":45}}},{"key":"group3","type":"added","value":{"deep":{"id":{"number":45}},"fee":100500}}]';
 export {
   correctStylishDiff,
   correctPlainDiff,
+  correctJSONDiff,
 };
--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -1,17 +1,38 @@
 import { expect, test } from '@jest/globals';
 import engineDiff from '../index.js';
-import { correctStylishDiff, correctPlainDiff } from '../__fixtures__/correctResults/correctResult.js'
+import { correctStylishDiff, correctPlainDiff, correctJSONDiff } from '../__fixtures__/correctResults/correctResult.js'
 
-test('stylish test', () => {
+test('stylish JSON test', () => {
   const filepath1 = 'file1.json';
   const filepath2 = 'file2.json';
 
   expect(engineDiff(filepath1, filepath2)).toBe(correctStylishDiff);
 });
 
-test('plain test', () => {
+test('plain JSON test', () => {
   const filepath1 = 'file1.json';
   const filepath2 = 'file2.json';
 
   expect(engineDiff(filepath1, filepath2, 'plain')).toBe(correctPlainDiff);
+});
+
+test('stylish YAML test', () => {
+  const filepath1 = 'file1.yml';
+  const filepath2 = 'file2.yml';
+
+  expect(engineDiff(filepath1, filepath2)).toBe(correctStylishDiff);
+});
+
+test('plain YAML test', () => {
+  const filepath1 = 'file1.yml';
+  const filepath2 = 'file2.yml';
+
+  expect(engineDiff(filepath1, filepath2, 'plain')).toBe(correctPlainDiff);
+});
+
+test('JSON test', () => {
+  const filepath1 = 'file1.json';
+  const filepath2 = 'file2.json';
+
+  expect(engineDiff(filepath1, filepath2, 'json')).toBe(correctJSONDiff);
 });
